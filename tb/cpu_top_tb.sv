98b7d0419b69d9ce7f68826f97ebd5e29cd8c01b
Vedant Desai <vedantdesai.work@gmail.com>
2026-07-30 12:27:45 +0530

    Implement execute stage and register write-back

--- tb/cpu_top_tb.sv
+++ tb/cpu_top_tb.sv
@@ -10,9 +10,14 @@ cpu_top dut (
     .rst(rst)
 );
 
-// Clock generation
+//---------------------------------------------------------
+// Clock Generation
+//---------------------------------------------------------
 always #5 clk = ~clk;
 
+//---------------------------------------------------------
+// Simulation
+//---------------------------------------------------------
 initial begin
 
     clk = 0;
@@ -21,34 +26,42 @@ initial begin
     #10;
     rst = 0;
 
-    // Let the CPU execute a few instructions
-    repeat (5) @(posedge clk);
-
-    $display("-------------------------------------------");
-    $display("CPU Execute Stage Test");
-    $display("-------------------------------------------");
-
-    $display("PC              = %0d", dut.pc);
-    $display("Instruction     = 0x%08h", dut.instruction);
-
     $display("");
+    $display("===============================================");
+    $display("        RV32I CPU EXECUTION TRACE");
+    $display("===============================================");
 
-    $display("Register Read 1 = %0d", dut.rd1);
-    $display("Register Read 2 = %0d", dut.rd2);
+    repeat (6) begin
 
-    $display("Immediate       = %0d", dut.immediate);
+        @(posedge clk);
 
-    $display("");
+        $display("-----------------------------------------------");
+        $display("Time         : %0t", $time);
+        $display("PC           : %0d", dut.pc);
+        $display("Instruction  : %08h", dut.instruction);
 
-    $display("ALU Control     = %b", dut.alu_control_signal);
-    $display("Operand B       = %0d", dut.operand_b);
+        $display("");
 
-    $display("");
+        $display("RS1 Value    : %0d", dut.rd1);
+        $display("RS2 Value    : %0d", dut.rd2);
+        $display("Immediate    : %0d", dut.immediate);
 
-    $display("ALU Result      = %0d", dut.alu_result);
+        $display("");
 
-    $display("-------------------------------------------");
+        $display("ALU Control  : %04b", dut.alu_control_signal);
+        $display("Operand B    : %0d", dut.operand_b);
+        $display("ALU Result   : %0d", dut.alu_result);
 
+        $display("");
+
+        $display("Registers");
+        $display("x1 = %0d", dut.rf.registers[1]);
+        $display("x2 = %0d", dut.rf.registers[2]);
+        $display("x3 = %0d", dut.rf.registers[3]);
+
+    end
+
+    $display("===============================================");
     $finish;
 
 end
