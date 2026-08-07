bdc0ab4c864b444bf42b9f7a44bd97f99195f22f
Vedant Desai <vedantdesai.work@gmail.com>
2026-08-07 22:43:18 +0530

    Verify AND instruction

--- rtl/memory/instruction_memory.sv
+++ rtl/memory/instruction_memory.sv
@@ -10,13 +10,19 @@ integer i;
 
 initial begin
 
-    // addi x1, x0, 123
-    memory[0] = 32'h07B00093;
+    // addi x1, x0, 12
+    memory[0] = 32'h00C00093;
+
+    // addi x2, x0, 10
+    memory[1] = 32'h00A00113;
+
+    // and x3, x1, x2
+    memory[2] = 32'h0020F1B3;
 
     // nop
-    memory[1] = 32'h00000013;
+    memory[3] = 32'h00000013;
 
-    for (i = 2; i < 256; i = i + 1)
+    for (i = 4; i < 256; i = i + 1)
         memory[i] = 32'h00000013;
 
 end
