651a87097a3ac76a7d7ae9c67276e36c3ee178f1
Vedant Desai <vedantdesai.work@gmail.com>
2026-08-09 18:52:16 +0530

    Verify SLTU instruction

--- rtl/memory/instruction_memory.sv
+++ rtl/memory/instruction_memory.sv
@@ -10,14 +10,14 @@ integer i;
 
 initial begin
 
-    // addi x1, x0, 10
-    memory[0] = 32'h00A00093;
+    // addi x1, x0, -1
+    memory[0] = 32'hFFF00093;
 
-    // addi x2, x0, 20
-    memory[1] = 32'h01400113;
+    // addi x2, x0, 1
+    memory[1] = 32'h00100113;
 
-    // slt x3, x1, x2
-    memory[2] = 32'h0020A1B3;
+    // sltu x3, x1, x2
+    memory[2] = 32'h0020B1B3;
 
     // nop
     memory[3] = 32'h00000013;
