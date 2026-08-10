1915b74e95d72d39f9b06814ab73d4600ca9d42e
Vedant Desai <vedantdesai.work@gmail.com>
2026-08-10 21:31:45 +0530

    Verify SB instruction

--- rtl/memory/instruction_memory.sv
+++ rtl/memory/instruction_memory.sv
@@ -13,13 +13,19 @@ initial begin
     // addi x1, x0, 0
     memory[0] = 32'h00000093;
 
-    // lw x2, 0(x1)
-    memory[1] = 32'h0000A103;
+    // addi x2, x0, 127
+    memory[1] = 32'h07F00113;
+
+    // sb x2, 0(x1)
+    memory[2] = 32'h00208023;
+
+    // lw x3, 0(x1)
+    memory[3] = 32'h0000A183;
 
     // nop
-    memory[2] = 32'h00000013;
+    memory[4] = 32'h00000013;
 
-    for (i = 3; i < 256; i = i + 1)
+    for (i = 5; i < 256; i = i + 1)
         memory[i] = 32'h00000013;
 
 end
