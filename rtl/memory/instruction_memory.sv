be3489157f0725d7dcfac365863c04cc4d531087
Vedant Desai <vedantdesai.work@gmail.com>
2026-08-11 20:28:58 +0530

    Verify BEQ instruction

--- rtl/memory/instruction_memory.sv
+++ rtl/memory/instruction_memory.sv
@@ -10,22 +10,27 @@ integer i;
 
 initial begin
 
-    // addi x1, x0, 0
-    memory[0] = 32'h00000093;
+    // addi x1, x0, 10
+    memory[0] = 32'h00A00093;
 
-    // lui x2, 0xABCDE
-    memory[1] = 32'hABCDE137;
+    // addi x2, x0, 10
+    memory[1] = 32'h00A00113;
 
-    // sw x2, 0(x1)
-    memory[2] = 32'h0020A023;
+    // beq x1, x2, +8
+    memory[2] = 32'h00208463;
 
-    // lw x3, 0(x1)
-    memory[3] = 32'h0000A183;
+    // This instruction should be skipped
+    // addi x3, x0, 99
+    memory[3] = 32'h06300193;
+
+    // Branch target
+    // addi x3, x0, 42
+    memory[4] = 32'h02A00193;
 
     // nop
-    memory[4] = 32'h00000013;
+    memory[5] = 32'h00000013;
 
-    for (i = 5; i < 256; i = i + 1)
+    for (i = 6; i < 256; i = i + 1)
         memory[i] = 32'h00000013;
 
 end
