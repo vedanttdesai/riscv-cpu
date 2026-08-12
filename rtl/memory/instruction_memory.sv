f60e8dde6d34e7a3d9bb27c9a2f6eeb9607cd662
Vedant Desai <vedantdesai.work@gmail.com>
2026-08-12 20:16:44 +0530

    Verify JALR instruction

--- rtl/memory/instruction_memory.sv
+++ rtl/memory/instruction_memory.sv
@@ -10,21 +10,24 @@ integer i;
 
 initial begin
 
-    // jal x3, +8
-    memory[0] = 32'h008001EF;
+    // addi x1, x0, 8
+    memory[0] = 32'h00800093;
+
+    // jalr x3, 0(x1)
+    memory[1] = 32'h000081E7;
 
     // This instruction should be skipped
     // addi x4, x0, 99
-    memory[1] = 32'h06300213;
+    memory[2] = 32'h06300213;
 
-    // Branch target
+    // Target: PC = 8
     // addi x4, x0, 42
-    memory[2] = 32'h02A00213;
+    memory[3] = 32'h02A00213;
 
     // nop
-    memory[3] = 32'h00000013;
+    memory[4] = 32'h00000013;
 
-    for (i = 4; i < 256; i = i + 1)
+    for (i = 5; i < 256; i = i + 1)
         memory[i] = 32'h00000013;
 
 end
